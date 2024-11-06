25b41949376d7c9e9c850b743aefb7672f11177d
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2024-11-05 18:10:33 -0600

    formatting

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const app = express();
  * VIEW ENGINE
  */
 
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(import.meta.dirname, "../client"));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(import.meta.dirname, "../client"));
 
 /**
  * MIDDLEWARES
@@ -25,56 +25,56 @@ app.use(express.json());
  */
 
 app.get("/", (req, res) => {
-  res.render("index");
+	res.render("index");
 });
 
 app.get("/join", (req, res) => {
-  const userId = uuidv7();
-  res.render("join-existing-room", { userId });
+	const userId = uuidv7();
+	res.render("join-existing-room", { userId });
 });
 
 app.get("/create", (req, res) => {
-  res.render("create-room");
+	res.render("create-room");
 });
 
 // url.com/chat/fooRoom?userId=foo&displayName=foo
 app.get("/chat/:roomId", (req, res) => {
-  const roomId = req.params?.roomId;
-  const { userId, displayName } = req.query;
-  console.log({roomId, userId, displayName });
+	const roomId = req.params?.roomId;
+	const { userId, displayName } = req.query;
+	console.log({ roomId, userId, displayName });
 
-  if (!roomId || !displayName) {
-    res.render("error", { error: "Something went wrong!" });
-    return;
-  }
+	if (!roomId || !displayName) {
+		res.render("error", { error: "Something went wrong!" });
+		return;
+	}
 
-  if (roomId === "test") {
-    if (!req.connection.server.ROOMS["test"]) {
-      req.connection.server.ROOMS["test"] = {};
-    }
-  } 
-  
-  if (!req.connection.server.ROOMS[roomId]) {
-    res.render("error", { error: "Something went wrong!" });
-    return;
-  }
+	if (roomId === "test") {
+		if (!req.connection.server.ROOMS["test"]) {
+			req.connection.server.ROOMS["test"] = {};
+		}
+	}
 
-  // If someone tries to join roomId with existing userId
-  if (req.connection.server.ROOMS[roomId][userId]) {
-    // If the displayName is diff it's prob a duplicate userId...
-    if (req.connection.server.ROOMS[roomId][userId].displayName !== displayName) {
-      console.log(`[/chat][ERROR] Duplicate userId's in single room!`, { roomId, userId, displayName });
-      res.render("error", { error: "Something went wrong!" });
-      return 
-    }
-  }
+	if (!req.connection.server.ROOMS[roomId]) {
+		res.render("error", { error: "Something went wrong!" });
+		return;
+	}
 
-  req.connection.server.ROOMS[roomId][userId] = { displayName };
-  res.render("chat-room", { displayName, roomId, members: req.connection.server.ROOMS[roomId] });
+	// If someone tries to join roomId with existing userId
+	if (req.connection.server.ROOMS[roomId][userId]) {
+		// If the displayName is diff it's prob a duplicate userId...
+		if (req.connection.server.ROOMS[roomId][userId].displayName !== displayName) {
+			console.log(`[/chat][ERROR] Duplicate userId's in single room!`, { roomId, userId, displayName });
+			res.render("error", { error: "Something went wrong!" });
+			return;
+		}
+	}
+
+	req.connection.server.ROOMS[roomId][userId] = { displayName };
+	res.render("chat-room", { displayName, roomId, members: req.connection.server.ROOMS[roomId] });
 });
 
 app.get("*", (req, res) => {
-  res.send("<h1>404 Not Found</h1>");
+	res.send("<h1>404 Not Found</h1>");
 });
 
 /**
@@ -82,7 +82,7 @@ app.get("*", (req, res) => {
  */
 
 const server = app.listen(process.env.EXPRESS_PORT, () => {
-  console.log(`Express app listening on port ${process.env.EXPRESS_PORT}!`);
+	console.log(`Express app listening on port ${process.env.EXPRESS_PORT}!`);
 });
 
 server.ROOMS = {};
--- wss/index.js
+++ wss/index.js
@@ -9,29 +9,29 @@ const wss = new WebSocketServer({ server });
 wss.on("connection", handleConnection);
 
 function handleConnection(ws, socket, request) {
-  console.log("[wss] connected");
-  ws.on("message", (message) => handleMessage(JSON.parse(message), ws));
+	console.log("[wss] connected");
+	ws.on("message", (message) => handleMessage(JSON.parse(message), ws));
 }
 
 function handleMessage(message, ws) {
-  switch(message?.type) {
-    case "message": {
-      const room = ws.room;
-      break;
-    }
-    case "": {
-      if (!message?.displayName) {
-        ws.send(JSON.stringify({ ok: false, message: "Missing display name" }));
-        break;
-      }
-      const roomId = uuidv7();
-      ws.room = roomId;
-      server.ROOMS[roomId] = [{ displayName: message.displayName, socket: ws }];
-      ws.send(JSON.stringify({ ok: true, roomId, message: "Room created successfully" }));
-      break;
-    }
-    default: {
-      console.log(`[wss][handleMessage] non existent type!`, {type: message?.type});
-    }
-  }
+	switch (message?.type) {
+		case "message": {
+			const room = ws.room;
+			break;
+		}
+		case "": {
+			if (!message?.displayName) {
+				ws.send(JSON.stringify({ ok: false, message: "Missing display name" }));
+				break;
+			}
+			const roomId = uuidv7();
+			ws.room = roomId;
+			server.ROOMS[roomId] = [{ displayName: message.displayName, socket: ws }];
+			ws.send(JSON.stringify({ ok: true, roomId, message: "Room created successfully" }));
+			break;
+		}
+		default: {
+			console.log(`[wss][handleMessage] non existent type!`, { type: message?.type });
+		}
+	}
 }
