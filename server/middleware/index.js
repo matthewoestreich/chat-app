f19e73ed76ee29c2a949a1509b820b2a8f347ee2
Matt Oestreich <21092343+oze4@users.noreply.github.com>
2024-11-12 00:11:53 -0600

    db changes
    added db pool so we don't need useDatabase middleware anymore

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,4 +1,3 @@
-export { default as useDatabase } from "./useDatabase.js";
 export { default as useCookieParser } from "./useCookieParser.js";
 export { default as useUserParamsFromBody } from "./useUserParamsFromBody.js";
 export { default as useCspNonce } from "./useCspNonce.js";
