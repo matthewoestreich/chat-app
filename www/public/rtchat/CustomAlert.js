d9f22083e5fe5bf33dfecda966530727fa63a4da
Matt Oestreich <matthewpoestreich@gmail.com>
2024-11-24 22:46:11 -0600

    had to revert
    extending HTMLElement as mobile didn't seem to like it.

--- www/public/rtchat/CustomAlert.js
+++ www/public/rtchat/CustomAlert.js
@@ -1,51 +1,44 @@
-// Custom HTML Element for status updates in a callout-like fashion.
-//
-// - Usage:
-//    // Use in DOM like a regular HTML element... (some attributes may not work as expected)
-//    <rtc-alert id="alert"></rtc-alert>
-//
-//    // Target element via JS.
-//    const alert = document.getElementById("alert");
-//
-//    alert.showSuccess("Yay!", "bi-check"); // must be a bootstrap icon
-//    --- or ---
-//    alert.showWarning(...); // same params as success
-//    --- or ---
-//    alert.showDanger(...);
-//
-// - Programmatically hide...
-//    alert.hide();
-//
-class CustomAlert extends HTMLElement {
-  constructor() {
-    super();
+// Allows you to reuse a single alert element..Returns a function that lets you hide alert.
+// Finds a button with data-bs-dismiss='alert' within alertEl, if found we use it to close alert.
+// If showing with icon, we expect it to be a bootstrap-icon as well as an <i> tag to be within your alertEl.
+function CustomAlert(alertEl, messageEl) {
+  this.isShown = false;
+  this.alertEl = alertEl;
+  this.messageEl = messageEl;
+  this.alertIconEl = undefined;
+  this.alertType = undefined;
+  this.closeButton = alertEl.querySelector("button[data-bs-dismiss='alert']");
+  if (this.closeButton) {
+    this.closeButton.addEventListener("click", this.close);
   }
-
-  showSuccess(msg, iconClass = "bi-check") {
-    this._render(msg, iconClass, "success");
-  }
-
-  showDanger(msg, iconClass = "bi-radioactive") {
-    this._render(msg, iconClass, "danger");
-  }
-
-  showWarning(msg, iconClass = "bi-exclamation") {
-    this._render(msg, iconClass, "warning");
-  }
-
-  hide() {
-    this.replaceChildren();
-  }
-
-  _render(msg, iconClass, alertType = "success") {
-    this.innerHTML = `
-      <div id="alert-display" class="alert alert-${alertType} d-flex flex-row align-items-center justify-content-between mh-100" role="alert">
-        <i class="bi ${iconClass}"></i>
-        <div id="alert-message" class="mb-0 max-h-100px overf-scroll">${msg}</div>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    `;
-  }
+  this.showWithIcon = function (alertType, message, icon) {
+    this.alertIconEl = alertEl.querySelector("i");
+    if (this.alertIconEl) {
+      this.alertIconEl.removeAttribute("class"); // clear classes before adding.
+      this.alertIconEl.classList.add("bi", icon);
+      this.messageEl.classList.add("ms-2"); // add margin since there will be an icon there now.
+    }
+    this.show(alertType, message);
+  };
+  this.show = function (alertType, message) {
+    if (this.isShown) {
+      this.messageEl.classList.remove("ms-2"); // remove just in case it exists..
+      // If already shown, close existing before showing new.
+      this.close();
+    }
+    this.alertType = `alert-${alertType}`;
+    this.alertEl.classList.add(this.alertType);
+    this.messageEl.innerText = message;
+    this.alertEl.classList.remove("d-none");
+    this.isShown = true;
+  };
+  this.close = function () {
+    if (this.closeButton) {
+      this.closeButton.removeEventListener("click", this.close);
+    }
+    this.alertEl.classList.remove(this.alertType);
+    this.messageEl.innerText = "";
+    this.alertEl.classList.add("d-none");
+    this.isShown = false;
+  };
 }
-
-customElements.define("rtc-alert", CustomAlert);
