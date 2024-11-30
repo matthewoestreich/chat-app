1667227008ec70beed8e77a155af85655dd4483d
Matt Oestreich <matthewpoestreich@gmail.com>
2024-11-30 15:00:02 -0600

    frontend
    - begin migrating to event driven design
    - reorg files
    - implement pug layouts

---
+++ www/public/rtchat/js/chat/ws.js
@@ -0,0 +1,215 @@
+// Add "emit" to WebSocket proto
+WebSocket.prototype.emitToServer = function (eventName, ...payload) {
+  this.send(JSON.stringify({ eventName, payload }));
+};
+
+const EVENT_TYPE = {
+  SEND_MESSAGE: "SEND_MESSAGE",
+  ERROR: "ERROR",
+  CREATE_ROOM: "CREATE_ROOM",
+  JOIN_ROOM: "JOIN_ROOM",
+  UNJOIN_ROOM: "UNJOIN_ROOM",
+  ENTER_ROOM: "ENTER_ROOM",
+  LEAVE_ROOM: "LEAVE_ROOM",
+  LIST_JOINABLE_ROOMS: "LIST_JOINABLE_ROOMS",
+  LIST_ROOMS: "LIST_ROOMS",
+  LIST_ROOM_MEMBERS: "LIST_ROOM_MEMBERS",
+  LIST_DIRECT_CONVERSATIONS: "LIST_DIRECT_CONVERSATIONS",
+  MEMBER_ENTERED_ROOM: "MEMBER_ENTERED_ROOM",
+  MEMBER_LEFT_ROOM: "MEMBER_LEFT_ROOM",
+  CONNECTION_ESTABLISHED: "CONNECTION_ESTABLISHED",
+  CONNECTION_CLOSED: "CONNECTION_CLOSED",
+};
+
+class WebSocketMessage {
+  type = undefined;
+  error = undefined;
+
+  static from(data) {
+    const { type, ...rest } = JSON.parse(String(data));
+    return new WebSocketMessage(type, rest);
+  }
+
+  constructor(type, dataOrError) {
+    this.type = type;
+    if (dataOrError instanceof Error || typeof dataOrError === "string") {
+      this.error = dataOrError;
+      return;
+    }
+    if (typeof dataOrError === "object" && dataOrError !== null) {
+      Object.assign(this, dataOrError);
+      return;
+    }
+  }
+}
+
+class WebSocketApp {
+  #parseRawMessage = (rawMessage) => {
+    if (!rawMessage?.data) {
+      return new WebSocketMessage(EVENT_TYPE.ERROR, new Error(`[ws][parseRawMessage] No data found on message.`));
+    }
+
+    const message = WebSocketMessage.from(rawMessage.data);
+
+    if (!message?.type) {
+      return new WebSocketMessage(EVENT_TYPE.ERROR, new Error(`[ws][parseRawMessage] No type found in message.`));
+    }
+    if ((!message.type) in EVENT_TYPE) {
+      return new WebSocketMessage(EVENT_TYPE.ERROR, new Error(`[ws][parseRawMessage] Message type not recognized. Got : '${message.type}'`));
+    }
+
+    return message;
+  };
+
+  /**
+   * URL for web socket server
+   * @param {string} webSocketUrl
+   */
+  constructor(webSocketUrl) {
+    if (!webSocketUrl) {
+      throw new Error(`WebSocketApp : webSocketUrl is required!`);
+    }
+
+    this.socket = new WebSocket(webSocketUrl);
+    this.listeners = {};
+
+    this.socket.onopen = () => {
+      console.log(`ws connected`);
+    };
+
+    this.socket.onclose = (ws, event) => {
+      console.log({ socket: "closed", ws, event });
+    };
+
+    this.socket.onmessage = (rawMessage) => {
+      const { error, type, ...data } = this.#parseRawMessage(rawMessage);
+      if (error) {
+        return this.emit(EVENT_TYPE.ERROR, error);
+      }
+      this.emit(type, this.socket, data);
+    };
+  }
+
+  on(eventName, listener) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName].push(listener);
+  }
+
+  emit(eventName, ...args) {
+    if (this.listeners[eventName]) {
+      this.listeners[eventName].forEach((listener) => listener(...args));
+    }
+  }
+
+  emitToServer(eventType, ...args) {
+    this.ws.emitToServer(eventType, ...args);
+  }
+}
+
+const wsapp = new WebSocketApp(WEBSOCKET_URL);
+
+wsapp.on(EVENT_TYPE.LIST_ROOMS, (data) => {
+  console.log(data);
+});
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+// ORIGINAL IMPL
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/*
+const ws = new WebSocket(WEBSOCKET_URL);
+
+ws.onopen = () => {
+  console.log(`ws connected`);
+};
+
+ws.onclose = (ws, event) => {
+  console.log({ socket: "closed", ws, event });
+};
+
+ws.onmessage = (rawMessage) => {
+  console.log({rawMessage})
+  const message = JSON.parse(rawMessage?.data);
+  if (!message?.type) {
+    return;
+  }
+
+  switch (message.type) {
+    case "ENTER_ROOM": {
+      const { members, messages } = message;
+      handleEnteredRoom(members, messages);
+      break;
+    }
+
+    case "LIST_ROOMS": {
+      handleRooms(roomsContainer, message.rooms);
+      break;
+    }
+
+    // Received a list of rooms that this user can select.
+    case "JOINABLE_ROOMS": {
+      const { ok, rooms, error } = message;
+      handleJoinableRooms(joinRoomModalRoomsContainer, ok, rooms, error);
+      break;
+    }
+
+    case "CREATE_ROOM": {
+      const { ok, rooms, createdRoomId, error } = message;
+      handleCreatedRoom(ok, rooms, createdRoomId, error);
+      break;
+    }
+
+    case "LIST_ROOM_MEMBERS": {
+      handleRoomMembers(membersContainer, message.members);
+      break;
+    }
+
+    // Got a chat message
+    case "SEND_MESSAGE": {
+      handleMessage(message);
+      break;
+    }
+
+    // Someone entered the room
+    case "member_entered": {
+      handleMemberEntered(message?.id);
+      break;
+    }
+
+    // Someone left the room we are currently in
+    case "member_left": {
+      handleMemberLeft(message?.id);
+      break;
+    }
+
+    case "joined": {
+      const { ok, error, rooms, joinedRoomId } = message;
+      handleJoinedRoom(ok, rooms, joinedRoomId, error);
+      break;
+    }
+
+    // You unjoined a room
+    case "unjoin": {
+      // { ok: bool, rooms?: <if ok, list of updated rooms to render>, error?: <if !ok, error will be here> }
+      const { ok, rooms, error } = message;
+      handleUnjoined(ok, rooms, error);
+      break;
+    }
+
+    case "get_direct_conversations": {
+      const { ok, conversations, isActive, error } = message;
+      handleDirectConversations(ok, conversations, error, directMessagesDrawerContainer);
+      break;
+    }
+
+    default: {
+      break;
+    }
+  }
+};
+*/
