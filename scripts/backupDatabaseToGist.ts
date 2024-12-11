cab2046905d75265df7d8aced2cc1ad2cd1514ff
Matt Oestreich <matthewpoestreich@gmail.com>
2024-12-11 00:03:41 -0600

    Update backupDatabaseToGist.ts

--- scripts/backupDatabaseToGist.ts
+++ scripts/backupDatabaseToGist.ts
@@ -12,27 +12,29 @@ if (require.main === module) {
 
 export default async function backupDatabaseToGist() {
   if (!process.env.GH_GISTS_API_KEY) {
-    error("[backupDbAndUploadGist] gists api key not found.");
-    return;
+    return error("[backupDbAndUploadGist] gists api key not found.");
   }
   if (!process.env.GIST_ID) {
-    error("[backupDbAndUploadGist] gist id not found");
-    return;
+    return error("[backupDbAndUploadGist] gist id not found");
   }
 
   try {
     log("[backupDbAndUploadGist][BEGIN]");
+
     // Backup db to file
     log(" -> Database backup started");
     await backupDatabase(DATABASE_PATH, BACKUP_FILE_PATH);
     log("  -> Database backup complete");
+
     log(" -> Uploading gist.");
     // Update our gist with new data
     await updateGist([BACKUP_FILE_PATH], process.env.GIST_ID, process.env.GH_GISTS_API_KEY);
     log("  -> Uploaded gist.");
+
     // Delete local backup file after uploading gist
     log("-> Removing backup file.");
     nodeFs.unlinkSync(BACKUP_FILE_PATH);
+
     log("\n[backupDbAndUploadGist][SUCCESS] Done.\n");
   } catch (e) {
     nodeFs.unlinkSync(BACKUP_FILE_PATH);
