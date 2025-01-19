dae039ab4f0e1a98d7ae47671f11325d649c74a3
Matt Oestreich <matthewpoestreich@gmail.com>
2025-01-18 23:59:53 -0600

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "cypress";
 import { config } from "dotenv";
 config();
 
-console.log(process.env);
-
 export default defineConfig({
   e2e: {
     baseUrl: `http://localhost:${process.env.EXPRESS_PORT}`,
