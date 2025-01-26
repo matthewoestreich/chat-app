ad1126f55c69aa0c5920b0b18b87fb0b236116ff
Matt Oestreich <matthewpoestreich@gmail.com>
2025-01-25 23:18:42 -0600

    Update attachDatabaseProvider.ts

--- server/middleware/attachDatabaseProvider.ts
+++ server/middleware/attachDatabaseProvider.ts
@@ -1,6 +1,31 @@
 import { Express, Request, Response, NextFunction } from "express";
 
-export default function attachDatabaseProvider(app: Express) {
+/**
+ * TLDR; This is a hacky way to dynamically set our DatabaseProvider middleware due to runtime
+ * decisions and being unable to add middleware after routes are processed
+ *
+ *
+ * Let me preface this by saying; this is very hacky.
+ *
+ * Our websocket implementation needs an http server passed to it. So we export our Express, app as well
+ * as our websocket impl, into our main entry point file.
+ *
+ * We attach a DatabaseProvider (database abstraction) to each `req` as `req.databaseProvider`. Selecting
+ * said provider happens at runtime based upon env variables.
+ *
+ * Due to the fact we are importing our Express (~/server/index.ts) app into entry point (~/index.ts),
+ * all of our Express middleware and routes are "processed" before we can set the DatabaseProvider.
+ *
+ * Essentially, we can't set our middleware after routes are processed.
+ *
+ * This is a hacky way to dynamically set that DatabaseProvider middleware.
+ *
+ * TODO come up with a better solution.
+ *
+ * @param app
+ * @returns {(provider: DatabaseProvider) => void}
+ */
+export default function attachDatabaseProvider(app: Express): (provider: DatabaseProvider) => void {
   let databaseProvider: DatabaseProvider | null = null;
 
   function setDatabaseProvider(provider: DatabaseProvider): void {
