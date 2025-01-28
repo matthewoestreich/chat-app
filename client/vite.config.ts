5a1f31751f9a91a21c709172d484923471fd1286
Matt Oestreich <matthewpoestreich@gmail.com>
2025-01-27 20:56:22 -0600

    begin adding react

---
+++ client/vite.config.ts
@@ -0,0 +1,19 @@
+import nodePath from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({ include: "**/*.{ts,js,tsx}" })],
+  base: "",
+  root: nodePath.resolve(__dirname, "./src"),
+  build: {
+    outDir: "../dist",
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+});
